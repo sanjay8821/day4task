2af7238fdff3d2b508f43f2413036264d77d4719
sanjay chandra sekar <136565613+sanjay8821@users.noreply.github.com>
2023-09-05 12:01:57 +0530

    Add files via upload

---
+++ day 4/task4.js
@@ -0,0 +1,61 @@
+let myResume={
+    basics: {
+      name: "SANJAY C",
+      email: "sanjay88y2k1@gmail.com",
+      phone: 9578741769,
+      degree: "bsc-cs",
+      location: {
+        address: "30/5 saivamuthia street",
+        postalCode: 600001,
+        city: "Chennai",
+        state: "Tamilnadu",
+        country: "India"
+      },
+      profiles: [
+        {
+          website: "https://www.linkedin.com/in/sanjay/",
+          github:"https://github.com/sanjay-sc"
+        }
+      ]
+    },
+    work: [
+      {
+        company: "null",
+        position: "null",
+        startDate: "null",
+        endDate: "nu;;",
+        summary: "null",
+      },
+    ],
+    education: [
+      {
+        institution: "dr mgr university",
+        department: "cs",
+        studyType: "fulltime",
+        "batch start year": 2021,
+        "batch end year": 2024,
+        "gpa": 6.5,
+      }
+    ],
+    "skills": [
+      {
+        name: "javascript,tally,html,css",
+        level: "beginer",
+        project: 
+          "upcoming"
+        
+      }
+    ],
+    languages: [
+      {
+        language: "Tamil,Enlish",
+      }
+    ],
+    interests: [
+      {
+        name: "script writter,eater,gaming",
+      }
+    ]
+  }
+  console.log(myResume);
+  
